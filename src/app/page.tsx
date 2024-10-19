05a367cccc851fdaa77714e38c4fcc695c84a1aa
Japsimar Soin <japsimarsoin2003@gmail.com>
2024-10-19 16:57:01 +0530

    Basic UI iwth mock data and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function HomePage() {
     <main className="">
        <div className="flex flex-wrap gap-4">
         {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-          <div key={image.id} className="w-48 h-40">
+          <div key={image.id} className="w-64 p-2 h-48">
             <img src={image.url} />
           </div>
         ))}
