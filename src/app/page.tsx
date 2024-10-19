135e43349e3ab8204a54df91195021bce921d706
Japsimar Soin <japsimarsoin2003@gmail.com>
2024-10-19 16:14:38 +0530

    Hosted app on Vercel and Added mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/wbWyzJnbAMB3hmQ1Ol0e4CGP9Kqgwb1iAVnFszX6SLmNTeUB",
+  "https://utfs.io/f/wbWyzJnbAMB3p5lFgdQCmwoFzgWh1Y0jbkNuZtcUaBn6IlQJ",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+       <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))}
+       </div>
     </main>
   );
 }
