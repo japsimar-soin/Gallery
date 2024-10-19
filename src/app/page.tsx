3965c2b74e936cd38e89d703c6165f67b12cdc49
Japsimar Soin <japsimarsoin2003@gmail.com>
2024-10-19 16:18:25 +0530

    Altered mock data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
 
 const mockUrls = [
-  "https://utfs.io/f/wbWyzJnbAMB3hmQ1Ol0e4CGP9Kqgwb1iAVnFszX6SLmNTeUB",
-  "https://utfs.io/f/wbWyzJnbAMB3p5lFgdQCmwoFzgWh1Y0jbkNuZtcUaBn6IlQJ",
+  "https://utfs.io/f/wbWyzJnbAMB3ZjNca4fTjiUOtblxKzRc3dBakP5DNygSfWEh",
+  "https://utfs.io/f/wbWyzJnbAMB3w4dqFWnbAMB3Of6L7pEWRP1sT0DGKaiuSxUo",
+  "https://utfs.io/f/wbWyzJnbAMB3JMUln470Clyi2UMqIxREjw5ODBWb6Jf4m097",
+  "https://utfs.io/f/wbWyzJnbAMB3p5lFgdQCmwoFzgWh1Y0jbkNuZtcUaBn6IlQJ"
 ];
 
 const mockImages = mockUrls.map((url, index) => ({
